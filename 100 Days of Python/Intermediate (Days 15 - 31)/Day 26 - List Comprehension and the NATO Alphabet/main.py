0e39f78aeecd8ff4b882ce38a8fbf617c562362c
MRedulla24 <m.redulla2024@gmail.com>
2024-07-11 11:17:52 +0800

    Added Day 26

---
+++ 100 Days of Python/Intermediate (Days 15 - 31)/Day 26 - List Comprehension and the NATO Alphabet/main.py
@@ -0,0 +1,15 @@
+# NATO Phonetic Alphabet
+# By Marco Redulla
+# Day 26 (11/07/2024)
+import pandas as pd
+
+CSVPATH = r"100 Days of Python\Intermediate (Days 15 - 31)\Day 26 - List Comprehension and the NATO Alphabet\nato_phonetic_alphabet.csv"
+phonetic_alphabet = pd.read_csv(CSVPATH)
+
+
+conversion = {row.letter:row.code for (index,row) in phonetic_alphabet.iterrows()}
+
+user_input = list(input("Enter a word:").upper())
+
+converted = [conversion[char] if char.isalpha() else " " for char in user_input]
+print(converted)
